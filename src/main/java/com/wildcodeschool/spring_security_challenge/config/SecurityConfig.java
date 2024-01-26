ac7eebff28705187320ac61e37c57e454cad07f2
arseniit <arseniit@gmail.com>
2024-01-26 17:52:31 +0100

    Rewrite security config. Remove deprecated code

--- src/main/java/com/wildcodeschool/spring_security_challenge/config/SecurityConfig.java
+++ src/main/java/com/wildcodeschool/spring_security_challenge/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.wildcodeschool.spring_security_challenge.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -38,20 +41,20 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> {
+        http.authorizeHttpRequests(authorize -> {
             try {
                 authorize
                         .requestMatchers("/avengers/assemble").hasRole("CHAMPION")
                         .requestMatchers("/secret-bases").hasRole( "DIRECTOR")
                         .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated()
-                        .and().formLogin()
-                        .and().httpBasic();
+                        .anyRequest().authenticated();
             } catch (Exception e) {
 
                 e.printStackTrace();
             }
-        });
+        })
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults());
         return http.build();
     }
 }
